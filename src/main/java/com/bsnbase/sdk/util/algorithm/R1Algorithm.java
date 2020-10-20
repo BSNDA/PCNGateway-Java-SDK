d81483c260d3917b57f47207e8eb6cbd674be82d
zhangqs <529535756@qq.com>
2020-10-20 17:53:48 +0800

    update fabric r1 algorithm

--- src/main/java/com/bsnbase/sdk/util/algorithm/R1Algorithm.java
+++ src/main/java/com/bsnbase/sdk/util/algorithm/R1Algorithm.java
@@ -1,15 +1,24 @@
 package com.bsnbase.sdk.util.algorithm;
 
 import com.bsnbase.sdk.util.common.UserCertInfo;
+import org.bouncycastle.asn1.ASN1Integer;
+import org.bouncycastle.asn1.DERSequenceGenerator;
+import org.bouncycastle.crypto.params.ECDomainParameters;
+import org.bouncycastle.crypto.params.ECPrivateKeyParameters;
+import org.bouncycastle.crypto.signers.ECDSASigner;
+import org.bouncycastle.jce.ECNamedCurveTable;
 import org.bouncycastle.jce.provider.BouncyCastleProvider;
+import org.bouncycastle.jce.spec.ECNamedCurveParameterSpec;
 import org.hyperledger.fabric.sdk.helper.Config;
 import org.hyperledger.fabric.sdk.security.CryptoPrimitives;
 import sun.security.pkcs10.PKCS10;
 
 import java.io.ByteArrayOutputStream;
 import java.io.PrintStream;
+import java.math.BigInteger;
 import java.security.*;
 import java.security.cert.Certificate;
+import java.security.interfaces.ECPrivateKey;
 import java.util.Base64;
 
 public class R1Algorithm implements AlgorithmTypeHandle {
@@ -20,14 +29,36 @@ public class R1Algorithm implements AlgorithmTypeHandle {
      */
     @Override
     public String sign(String stringPrivateKey, String signString) throws Exception {
-
         CryptoPrimitives cryptoPrimitives = new CryptoPrimitives();
         cryptoPrimitives.init();
         PrivateKey privateKey = cryptoPrimitives.bytesToPrivateKey(stringPrivateKey.getBytes());
+        ECPrivateKey eck = (ECPrivateKey) privateKey;
 
-        byte[] signature = cryptoPrimitives.sign(privateKey, signString.getBytes());
 
-        return java.util.Base64.getEncoder().encodeToString(signature);
+        ECNamedCurveParameterSpec spec = ECNamedCurveTable.getParameterSpec("secp256r1");
+        ECDomainParameters domain = new ECDomainParameters(spec.getCurve(), spec.getG(), spec.getN());
+        ECDSASigner signer = new ECDSASigner();
+        ECPrivateKeyParameters parameters = new ECPrivateKeyParameters(eck.getS(), domain);
+        signer.init(true, parameters);
+        byte[] hash = hash(signString.getBytes());
+        BigInteger[] signature = signer.generateSignature(hash);
+
+        BigInteger r = signature[0];
+        BigInteger s = signature[1];
+
+        // BIP62: "S must be less than or equal to half of the Group Order N"
+
+        BigInteger overTwo = domain.getN().divide(new BigInteger("2"));
+        if (s.compareTo(overTwo) == 1) {
+            s = domain.getN().subtract(s);
+        }
+
+        ByteArrayOutputStream bos = new ByteArrayOutputStream();
+        DERSequenceGenerator seq = new DERSequenceGenerator(bos);
+        seq.addObject(new ASN1Integer(r));
+        seq.addObject(new ASN1Integer(s));
+        seq.close();
+        return java.util.Base64.getEncoder().encodeToString(bos.toByteArray());
     }
 
     /**
@@ -85,6 +116,21 @@ public class R1Algorithm implements AlgorithmTypeHandle {
         user.setKey(privateKey);
         return user;
     }
+    public static byte[] hash(byte[] input) {
+        return hash(input, 0, input.length);
+    }
 
+    public static byte[] hash(byte[] input, int offset, int length) {
+        MessageDigest digest = newDigest();
+        digest.update(input, offset, length);
+        return digest.digest();
+    }
+    public static MessageDigest newDigest() {
+        try {
+            return MessageDigest.getInstance("SHA-256");
+        } catch (NoSuchAlgorithmException e) {
+            throw new RuntimeException(e);  // Can't happen.
+        }
+    }
 
 }
