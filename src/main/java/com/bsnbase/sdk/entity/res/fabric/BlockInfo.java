aa53bbc7b0d78278bbb892968eaee579e8dd8786
zhangqs <529535756@qq.com>
2021-10-14 10:32:03 +0800

    fix class name

---
+++ src/main/java/com/bsnbase/sdk/entity/res/fabric/BlockInfo.java
@@ -0,0 +1,11 @@
+package com.bsnbase.sdk.entity.res.fabric;
+
+import lombok.Data;
+
+@Data
+public class BlockInfo {
+    String txId;
+    String blockHash;
+    Integer status;
+
+}
---
+++ src/main/java/com/bsnbase/sdk/entity/res/fabric/CcRes.java
@@ -0,0 +1,9 @@
+package com.bsnbase.sdk.entity.res.fabric;
+
+import lombok.Data;
+
+@Data
+public class CcRes {
+    Integer ccCode;
+    String ccData;
+}
