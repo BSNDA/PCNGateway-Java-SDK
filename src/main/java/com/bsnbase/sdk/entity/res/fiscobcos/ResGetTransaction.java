1eca83813be9303f0b8c6ff859955382d524fb36
zhangqingsong <529535756@qq.com>
2021-11-11 13:33:54 +0800

    Update ResGetTransaction.java

--- src/main/java/com/bsnbase/sdk/entity/res/fiscobcos/ResGetTransaction.java
+++ src/main/java/com/bsnbase/sdk/entity/res/fiscobcos/ResGetTransaction.java
@@ -8,20 +8,20 @@ public class ResGetTransaction implements IBody{
     String txId;
     String blockHash;
     int blockNumber;
-	int gasUsed;
+    int gasUsed;
     String from;
     String to;
-	int value;
-	String input;
-	@Override
-	public String getEncryptionValue() {
-		return (this.txId == null?"":this.txId)
-				+ (this.blockHash == null?"":this.blockHash)
-				+ (this.blockNumber)
-				+ (this.gasUsed)
-				+ (this.from == null?"":this.from)
-				+ (this.to == null?"":this.to)
-				+ (this.value)
-				+ (this.input == null?"":this.input);
+    int value;
+    String input;
+    @Override
+    public String getEncryptionValue() {
+	return (this.txId == null?"":this.txId)
+		+ (this.blockHash == null?"":this.blockHash)
+		+ (this.blockNumber)
+		+ (this.gasUsed)
+		+ (this.from == null?"":this.from)
+		+ (this.to == null?"":this.to)
+		+ (this.value)
+		+ (this.input == null?"":this.input);
 	}
 }
